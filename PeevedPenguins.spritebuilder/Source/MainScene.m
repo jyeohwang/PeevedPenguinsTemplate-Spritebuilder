070aaef41b328d3ccf2d6a33e23092499272747a
Jenny Yeoh-Wang <jyyw1644@Macintosh-75.local>
2014-06-25 13:22:01 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/MainScene.m
+++ PeevedPenguins.spritebuilder/Source/MainScene.m
@@ -12,8 +12,6 @@
 
 - (void)play {
     CCLOG(@"play button pressed");
-    CCScene *gameplayScene = [CCBReader loadAsScene:@"Gameplay"];
-    [[CCDirector sharedDirector] replaceScene:gameplayScene];
 }
 
 @end
