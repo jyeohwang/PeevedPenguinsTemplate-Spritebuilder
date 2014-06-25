c48eb73f964319439e6513b36e64098c9349e88e
Jenny Yeoh-Wang <jyyw1644@Macintosh-75.local>
2014-06-25 13:52:50 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.h
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -10,8 +10,8 @@
 
 @interface Gameplay : CCNode
 
-- (void)didLoadFromCCB; 
-- (void)touchBegan;
-- (void)launchPenguin;
+//- (void)didLoadFromCCB; 
+//- (void)touchBegan;
+//- (void)launchPenguin;
 
 @end
